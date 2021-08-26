ea86659ad994c6d7adc5861fcfe434f8724b89e5
matthewmcpolinkainos <88448227+matthewmcpolinkainos@users.noreply.github.com>
2021-08-26 14:12:10 +0100

    unused imports

--- src/test/java/JobRolesTest.java
+++ src/test/java/JobRolesTest.java
@@ -1,19 +1,6 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.kainos.ea.WebServiceApplication;
-import com.kainos.ea.WebServiceConfiguration;
 import com.kainos.ea.objects.JobRole;
-import com.kainos.ea.resources.DBTest;
-import io.dropwizard.jackson.Jackson;
-import io.dropwizard.testing.junit5.DropwizardAppExtension;
-import io.dropwizard.testing.junit5.DropwizardExtensionsSupport;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-
-import javax.ws.rs.core.Response;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
+import io.dropwizard.jackson.Jackson;;
 
 import static io.dropwizard.testing.FixtureHelpers.fixture;
 import static org.assertj.core.api.Assertions.assertThat;
