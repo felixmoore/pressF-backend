fd7286b81a67234250d74c9ff85d5ac4ae50646f
Felix Moore <felixmoore115@gmail.com>
2021-08-31 14:36:57 +0100

    Removed ResourceExtension testing

--- src/test/java/JobRolesResourceTest.java
+++ src/test/java/JobRolesResourceTest.java
@@ -1,4 +1,6 @@
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.verify;
@@ -8,9 +10,8 @@ import com.kainos.ea.db.JobRolesDAO;
 import com.kainos.ea.objects.JobRole;
 import com.kainos.ea.resources.JobRolesResource;
 import io.dropwizard.testing.junit5.DropwizardExtensionsSupport;
-import io.dropwizard.testing.junit5.ResourceExtension;
 import java.util.Optional;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.NotFoundException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,14 +19,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 
 @ExtendWith(DropwizardExtensionsSupport.class)
 class JobRolesResourceTest {
-  private static final JobRolesDAO jobRolesDAO = mock(JobRolesDAO.class);
-  private static final ResourceExtension EXT = ResourceExtension.builder()
-      .addResource(new JobRolesResource(jobRolesDAO))
-      .build();
+  private static JobRolesDAO jobRolesDAO;
+  private JobRolesResource resource;
   private JobRole jobRole;
 
   @BeforeEach
   void setup() {
+    jobRolesDAO = mock(JobRolesDAO.class);
+    resource = new JobRolesResource(jobRolesDAO);
     jobRole = new JobRole();
     jobRole.setJobRoleID(1);
   }
@@ -39,18 +40,21 @@ class JobRolesResourceTest {
   void getJobRoleDetailsSuccess() {
     when(jobRolesDAO.getJobRoleDetails(1)).thenReturn(Optional.of(jobRole));
 
-    JobRole found = EXT.target("/api/getjobroledetails/1").request().get(JobRole.class);
+    JobRole result = resource.getJobRoleDetails(1);
+    assertThat(result.getJobRoleID()).isEqualTo(jobRole.getJobRoleID());
 
-    assertThat(found.getJobRoleID()).isEqualTo(jobRole.getJobRoleID());
     verify(jobRolesDAO).getJobRoleDetails(1);
   }
 
   @Test
   void getPersonNotFound() {
     when(jobRolesDAO.getJobRoleDetails(8)).thenReturn(Optional.empty());
-    final Response response = EXT.target("/api/getjobroledetails/8").request().get();
 
-    assertThat(response.getStatusInfo().getStatusCode()).isEqualTo(Response.Status.NOT_FOUND.getStatusCode());
+    Exception e = assertThrows(NotFoundException.class, () -> {
+      JobRole result = resource.getJobRoleDetails(8);
+    });
+
+    assertTrue(e.getMessage().contains(("Job role not found.")));
     verify(jobRolesDAO).getJobRoleDetails(8);
   }
 }
