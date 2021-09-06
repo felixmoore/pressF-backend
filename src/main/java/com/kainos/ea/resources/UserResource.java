00093b68390e9dc72017bc1b8d42d5cb59963232
Felix Moore <felixmoore115@gmail.com>
2021-09-06 11:08:41 +0100

    User login route updated

--- src/main/java/com/kainos/ea/resources/UserResource.java
+++ src/main/java/com/kainos/ea/resources/UserResource.java
@@ -1,12 +1,15 @@
 package com.kainos.ea.resources;
 
 import com.kainos.ea.db.UserDAO;
+import com.kainos.ea.objects.User;
+import io.dropwizard.auth.Auth;
 import io.swagger.annotations.Api;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-;
+
 
 /**
  * Defines API endpoints for the User section of the system.
@@ -19,5 +22,15 @@ public class UserResource {
 
   public UserResource(UserDAO userDAO) {
     this.userDAO = userDAO;
+  }
+
+  @POST
+  @Path("/login")
+  @Produces(MediaType.APPLICATION_JSON)
+  public User getLoginResponse(@Auth User user) {
+    //authenticate user
+    //return access token
+    //return error if not authenticated
+    return user;
   }
 }
